7c6baa67c570bfe08ae7a3def146226153118b0d
Your Name <nguyenducanhtuan0602@gmail.com>
2023-11-16 11:44:01 +0700

    add di trom

--- views/src/APIs/ServerAPI/BaseServerApi.js
+++ views/src/APIs/ServerAPI/BaseServerApi.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 const BaseServerApi = axios.create({
   //baseURL: "http://192.168.1.9:5000",
-   baseURL: "http://localhost:5000",
+   baseURL: "http://localhost:5050",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-export const baseURL = "http://localhost:5000";
+export const baseURL = "http://localhost:5050";
 
 export default BaseServerApi;
